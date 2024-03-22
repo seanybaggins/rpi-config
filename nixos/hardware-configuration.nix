deb3f24ae0ff198f3b4a4246184d8c6d27f31fb2
Sean Link <seanybaggins1@gmail.com>
2024-03-21 18:50:34 -0600

    About to overwrite hardware-configuration.nix

--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -1,14 +1,18 @@
 # This is just an example, you should generate yours with nixos-generate-config and put it in here.
 {
   fileSystems."/boot" = {
-    device = "/dev/disk/by-label/FIRMWARE";
+    device = "/dev/disk/by-label/Boot";
     fsType = "vfat";
   };
 
   fileSystems."/" = {
-    device = "/dev/disk/by-label/NIXOS_SD";
+    device = "/dev/disk/by-label/main";
     fsType = "ext4";
   };
+  
+  swapDevices = [{
+    device = "/dev/sda3";
+  }];
 
   # Set your system kind (needed for flakes)
   nixpkgs.hostPlatform = "aarch64-linux";
